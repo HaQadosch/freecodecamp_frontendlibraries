3e151b67bf20480202f1a47399ff28f0df599e19
HaQadosch Berraka <haqadosch@pm.me>
2020-01-04 00:02:14 +0000

    Add AC onClick functionality
    Only regarding the total elt, will need to add the history reset as well.

--- Projects/calculator/src/Buttons/Clear.tsx
+++ Projects/calculator/src/Buttons/Clear.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { clear } from "../Store/rootReducer";
 
 export const Clear: React.FC = () => {
+  const dispatch = useDispatch()
+
   return (
-    <button id="clear">
+    <button id="clear" onClick={ handleClick }>
       AC
     </button>
   )
+
+  function handleClick () {
+    dispatch(clear())
+  }
 }
+
+/**
+ * {type: 'total/setTotal', payload: { value: 1954 }}
+ */
--- Projects/calculator/src/Store/rootReducer.ts
+++ Projects/calculator/src/Store/rootReducer.ts
@@ -8,4 +8,4 @@ export const rootReducer = combineReducers({
 })
 
 export type RootState = ReturnType<typeof rootReducer>
-console.log(setTotal, clear)
+export { setTotal, clear }
