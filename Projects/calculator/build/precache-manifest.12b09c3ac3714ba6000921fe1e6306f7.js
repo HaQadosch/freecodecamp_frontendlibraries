b6a7cdd21e405259e2150fc24c448544e2e62f0b
HaQadosch Berraka <haqadosch@pm.me>
2020-01-09 22:20:21 +0000

    adding build from calculator

---
+++ Projects/calculator/build/precache-manifest.12b09c3ac3714ba6000921fe1e6306f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8767d7f825473ee25127456f598c174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5db150adf243a7b6f07e",
+    "url": "/static/css/main.595217ca.chunk.css"
+  },
+  {
+    "revision": "aa1a2bc297643563f93e",
+    "url": "/static/js/2.ded2f6db.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.ded2f6db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5db150adf243a7b6f07e",
+    "url": "/static/js/main.2c100c4c.chunk.js"
+  },
+  {
+    "revision": "37e9f1103c37e420f275",
+    "url": "/static/js/runtime-main.5bcf610d.js"
+  }
+]);
